e2a82b0c5331dc7ab670a11803531a4e4d879802
import-min <minahalaisha@gmail.com>
2026-04-11 13:21:04 -0400

    Update __init__.py

--- src/fairprs_clin/__init__.py
+++ src/fairprs_clin/__init__.py
@@ -1,2 +1,12 @@
-__all__ = ['__version__']
-__version__ = '0.1.0'
+from .equity import (
+    pairwise_smds, ks_tests, bootstrap_group_stats,
+    flagging_disparity, sensitivity_curve, equalized_cutoffs,
+)
+
+__version__ = '0.2.0'
+
+__all__ = [
+    '__version__',
+    'pairwise_smds', 'ks_tests', 'bootstrap_group_stats',
+    'flagging_disparity', 'sensitivity_curve', 'equalized_cutoffs',
+]
