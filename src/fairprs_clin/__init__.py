4ac6a2d46b932d67f730787699c9c0eacee9926a
import-min <minahalaisha@gmail.com>
2026-04-17 17:40:38 -0400

    Update __init__.py

--- src/fairprs_clin/__init__.py
+++ src/fairprs_clin/__init__.py
@@ -1,12 +1,18 @@
 from .equity import (
     pairwise_smds, ks_tests, bootstrap_group_stats,
     flagging_disparity, sensitivity_curve, equalized_cutoffs,
+    resource_constrained_fair_threshold,
 )
+from .portability import aps_distributional, aps_clinical, bootstrap_aps
+from .recalibration import bayesian_group_recalibration, evaluate_recalibration
 
-__version__ = '0.2.0'
+__version__ = '0.4.0'
 
 __all__ = [
     '__version__',
     'pairwise_smds', 'ks_tests', 'bootstrap_group_stats',
     'flagging_disparity', 'sensitivity_curve', 'equalized_cutoffs',
+    'resource_constrained_fair_threshold',
+    'aps_distributional', 'aps_clinical', 'bootstrap_aps',
+    'bayesian_group_recalibration', 'evaluate_recalibration',
 ]
